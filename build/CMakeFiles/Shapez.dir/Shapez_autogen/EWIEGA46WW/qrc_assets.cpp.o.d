8cfb6fc66d3f4b37de79161ba47cf9dd671b237a
Liu YuXuan <231880433@smail.nju.edu.cn>
2026-04-09 18:59:11 +0800

    Remove autogenerated files from build directory

--- build/CMakeFiles/Shapez.dir/Shapez_autogen/EWIEGA46WW/qrc_assets.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/Shapez.dir/Shapez_autogen/EWIEGA46WW/qrc_assets.cpp.o: \
-  /Users/chenxin/Library/Mobile\ Documents/com~apple~CloudDocs/Workspace/C++/shapez/build/Shapez_autogen/EWIEGA46WW/qrc_assets.cpp
